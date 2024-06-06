bc75d2d23b5d99b1546b225ca8f2b1a2d740bb9b
amin <161345340+55amin@users.noreply.github.com>
2024-06-06 22:34:50 +0100

    Add download and run functionality

--- youloader.py
+++ youloader.py
@@ -11,6 +11,19 @@ app = customtkinter.CTk()
 app.geometry('1920x1080')
 app.title('YouLoader')
 
+# Download
+def start_download():
+    try:
+        yt_link = link.get()
+        yt_func = YouTube(yt_link)
+        video = yt_func.streams.get_highest_resolution()
+        title.configure(text=yt_func.title)
+        finish_download.configure(text='')
+        video.download()
+        finish_download.configure(text='Download complete', text_color='green')
+    except:
+        finish_download.configure(text='Download unsuccessful', text_color='red')
+
 # UI Elements
 title = customtkinter.CTkLabel(app, text='YouLoader\nInsert a YouXXXX link to download')
 title.pack(padx=10, pady=10)
@@ -24,3 +37,5 @@ download.pack(padx=10, pady=10)
 finish_download = customtkinter.CTkLabel(app, text='')
 finish_download.pack()
 
+# Run App
+app.mainloop()
