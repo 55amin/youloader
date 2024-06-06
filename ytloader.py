bd84a151ba0cc11c2bf2be7c0b977f9cfbfcdfb0
amin <161345340+55amin@users.noreply.github.com>
2024-06-06 22:32:43 +0100

    Add system settings and app frame

---
+++ ytloader.py
@@ -0,0 +1,13 @@
+import tkinter
+import customtkinter
+from pytube import youtube
+
+# System Settings
+customtkinter.set_appearance_mode('system')
+customtkinter.set_default_color_theme('blue')
+
+# App Frame
+app = customtkinter.CTk()
+app.geometry('1920x1080')
+app.title('YouLoader')
+
