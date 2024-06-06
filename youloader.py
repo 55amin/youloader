bae0f7bba007e164ef60f4485e84483f7a82d0c1
amin <161345340+55amin@users.noreply.github.com>
2024-06-06 22:50:24 +0100

    Add audio download functionality

--- youloader.py
+++ youloader.py
@@ -12,7 +12,7 @@ app.geometry('1920x1080')
 app.title('YouLoader')
 
 # Download
-def start_download():
+def download_mp4():
     try:
         yt_link = link.get()
         yt_func = YouTube(yt_link)
@@ -24,6 +24,18 @@ def start_download():
     except:
         finish_download.configure(text='Download unsuccessful', text_color='red')
 
+def download_sound():
+    try:
+        yt_link = link.get()
+        yt_func = YouTube(yt_link)
+        audio = yt_func.streams.get_audio_only()
+        title.configure(text=yt_func.title)
+        finish_download.configure(text='')
+        audio.download()
+        finish_download.configure(text='Download complete', text_color='green')
+    except:
+        finish_download.configure(text='Download unsuccessful', text_color='red')
+        
 # UI Elements
 title = customtkinter.CTkLabel(app, text='YouLoader\nInsert a YouXXXX link to download')
 title.pack(padx=10, pady=10)
@@ -32,8 +44,10 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=720, height=60, textvariable=url_var)
 link.pack()
 
-download = customtkinter.CTkButton(app, text='Download', command=start_download)
-download.pack(padx=10, pady=10)
+download_video = customtkinter.CTkButton(app, text='Download as video', command=download_mp4)
+download_video.pack(padx=10, pady=10)
+download_audio = customtkinter.CTkButton(app, text='Download as audio', command=download_sound)
+download_audio.pack(padx=10, pady=10)
 finish_download = customtkinter.CTkLabel(app, text='')
 finish_download.pack()
 
